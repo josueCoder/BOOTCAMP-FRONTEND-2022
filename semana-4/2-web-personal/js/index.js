18b578910d1c159ffc8c8b3570950db78d7072bb
rody josue <rody.josue.cc.c@gmail.com>
2022-03-27 17:50:34 -0500

    add funcionalidad al menu con js

--- semana-4/2-web-personal/js/index.js
+++ semana-4/2-web-personal/js/index.js
@@ -2,17 +2,18 @@
 
 const documentReady=()=>{
 
+const headerNavMenu=document.getElementById("headerNavMenu");
+
+    const openMenu=()=>{
+        const headerNavLinkLists=document.getElementById("headerNavLinkLists");
+        headerNavLinkLists.classList.toggle("header-nav__link-lists--left-0");
 
 
+    }
 
 
+    headerNavMenu.addEventListener("click",openMenu);
 
-
-
-
-
-
-    
 
 }
 
