81f342481ad65575c2ae573c32fd05abdda18217
rody josue <rody.josue.cc.c@gmail.com>
2022-03-29 12:19:29 -0500

    add funcionalidad a los item menu en mobile

--- semana-4/2-web-personal/js/index.js
+++ semana-4/2-web-personal/js/index.js
@@ -2,17 +2,43 @@
 
 const documentReady=()=>{
 
-const headerNavMenu=document.getElementById("headerNavMenu");
+    const header=document.getElementById("header");
+    const headerNavMenuIcon=document.getElementById("headerNavMenuIcon");   
+    const headerNavMenu=document.getElementById("headerNavMenu");
+    const headerNavLinks=[...document.querySelectorAll(".header-nav__link")];
+    const headerNavLinkListsLinkItems=[...document.querySelectorAll(".header-nav__link-lists__link-item")];
 
-    const openMenu=()=>{
+    console.log(headerNavLinkListsLinkItems);
+
+
+    const headerScroll=()=>{
+        header.classList.toggle("header--scroll",window.scrollY>0);
+        headerNavMenuIcon.classList.toggle("header-nav__menu-icon--scroll",window.scrollY>0);
+        headerNavLinks.forEach((element)=>{
+            element.classList.toggle("header-nav__link--scroll",window.scrollY>0);
+
+        });
+
+    }
+
+
+    const toggleMenu=()=>{
         const headerNavLinkLists=document.getElementById("headerNavLinkLists");
         headerNavLinkLists.classList.toggle("header-nav__link-lists--left-0");
 
 
     }
+    
+    headerNavLinkListsLinkItems.map((element)=>{
+        element.addEventListener("click",toggleMenu);
+    })
+    
 
 
-    headerNavMenu.addEventListener("click",openMenu);
+    headerNavMenu.addEventListener("click" ,toggleMenu);
+    window.addEventListener("scroll",headerScroll);
+   
+   
 
 
 }
